5d0ee4af8281d64e2253f61211830d8569de8bfb
Rei-Yi Tan <tanreiyi@gmail.com>
2023-05-06 23:48:12 -0500

    Updates

--- src/components/functions/index.js
+++
@@ -1,9 +0,0 @@
-export { default as loadPlaylistSongs } from "./loadPlaylistSongs";
-export { default as loadPlaylists } from "./loadPlaylists";
-export { default as loadAllSongs } from "./loadAllSongs";
-export { default as createPlayback } from "./createPlayback";
-export { default as isPlaying } from "./isPlaying";
-export { default as slicePlaylist } from "./slicePlaylist";
-export { default as songsAreEqual } from "./songsAreEqual";
-export { default as shuffleArray } from "./shuffleArray";
-export { default as createQueue } from "./createQueue";
